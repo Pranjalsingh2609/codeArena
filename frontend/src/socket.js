730017805eaf1e549d7fa3f370112c20655cfc55
kanvamotu <pranjalsingh2609@gmail.com>
2026-04-06 08:40:14 +0530

    fix: added socket auth token

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,4 +2,8 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = "https://codearena-az4r.onrender.com";
 
-export const socket = io(SOCKET_URL);
+export const socket = io(SOCKET_URL, {
+  auth: {
+    token: localStorage.getItem("token") // or wherever you store JWT
+  }
+});
